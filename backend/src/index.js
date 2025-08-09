9fb563584ee0f35cf2a5e8ad66f77639166299e5
Luquinh4449c <lucasbr8866@gmail.com>
2025-08-09 18:38:22 -0300

    :closed_lock_with_key: feat:(login) cria rota de login com token jwt

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import createUserRoutes from './src/routes/createuser.js';
-import connectDB from './src/services/moongoseService.js';
+import createUserRoutes from './routes/createuser.js';
+import authRoutes from './routes/auth.js';
+import connectDB from './services/moongoseService.js';
 
 const app = express();
 app.use(express.json());
@@ -11,9 +12,8 @@ console.log('URI carregada:', process.env.MONGO_URI); // deve mostrar a URI comp
 
 connectDB();
 
-app.use('/Users', createUserRoutes );
-
-
+app.use('/Users', createUserRoutes ); // Rota para criação de usuários
+app.use('/auth', authRoutes); // Rota para autenticação
 
 const PORT = 3001;
 
---
+++ backend/src/routes/auth.js
@@ -0,0 +1,48 @@
+import express from "express";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from '../models/User.js';
+import { JWT_SECRET } from '../config.js';
+
+
+const router = express.Router();
+
+
+
+router.post('/', async (req, res) => {
+  const { email, password } = req.body;
+
+  // Verifique se o e-mail e a senha foram fornecidos
+  if (!email || !password) {
+    return res.status(400).json({ message: "E-mail e senha são obrigatórios" });
+  }
+
+  try {
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Credenciais inválidas" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({ message: "Credenciais inválidas" });
+    }
+    console.log("JWT_SECRET:", JWT_SECRET);
+
+
+    const token = jwt.sign({ userId: user._id }, JWT_SECRET, { expiresIn: "1h" });
+    res.json({ token });
+
+
+  } catch (err) {
+    console.error("Erro ao fazer login:", err);
+    res.status(500).json({ message: "Erro interno do servidor" });
+  }
+  
+
+});
+
+export default router;
